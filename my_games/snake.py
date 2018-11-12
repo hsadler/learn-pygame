714a5a1a3fa7f109e84e085bed27c21925ca91d9
Harry Sadler <harrysadlermusic@gmail.com>
2018-11-11 19:33:21 -0800

    move snake game logic to Game class

--- my_games/snake.py
+++ my_games/snake.py
@@ -11,41 +11,17 @@ from snake_lib.snake import Snake
 
 def main():
 
-	# game
-	game = Game(pygame=pygame, config=config)
-	# grid
+	# instantiate game objects
 	grid = Grid(game=game)
+
+	# instantiate game manager
+	game = Game(pygame=pygame, config=config)
 	# initialize with entire display update
-	game.pygame.display.update()
+	game.init_display()
 
 	# game loop
 	while game.running:
-
-		# exit event conditions
-		for event in game.pygame.event.get():
-			if event.type == game.pygame.QUIT:
-				game.set_running(False)
-			if event.type == game.pygame.KEYDOWN:
-				if event.key == game.pygame.K_ESCAPE or event.unicode == 'q':
-					game.set_running(False)
-
-		# # block selection based on user input
-		# direction = grid.get_direction_from_user_input()
-		# if direction is not None:
-		# 	new_selected_block = grid.get_adjacent_block_from_selected(
-		# 		direction=direction
-		# 	)
-		# 	if new_selected_block:
-		# 		grid.select_block(new_selected_block)
-
-		# # update game objects
-		# grid.update_selected()
-
-		# # update display only selected block
-		# pygame.display.update(grid.get_updated_rect())
-
-		# # lock game loop rate
-		# game_clock.tick(GAME_LOOP_RATE)
+		game.run()
 
 
 if __name__=="__main__":
--- my_games/snake_lib/game.py
+++ my_games/snake_lib/game.py
@@ -4,7 +4,7 @@
 
 class Game():
 
-	def __init__(self, pygame, config):
+	def __init__(self, pygame, config, grid=None):
 		self.config = config
 		self.pygame = pygame
 		self.pygame.init()
@@ -19,13 +19,38 @@ class Game():
 		self.running = True
 		self.updated = []
 
+	def run(self):
+		"""
+		game loop main method
+		"""
+
+		# exit event conditions
+		for event in self.pygame.event.get():
+			if event.type == self.pygame.QUIT:
+				self.running = False
+			if event.type == self.pygame.KEYDOWN:
+				if event.key == self.pygame.K_ESCAPE or event.unicode == 'q':
+					self.running = False
+
+		# block selection based on user input
+		direction = self.get_direction_from_user_input()
+		if direction is not None:
+			print('direction:')
+			print(direction)
+
+		# lock game loop rate
+		self.clock.tick(self.config.GAME_LOOP_RATE)
+
+	def get_direction_from_user_input(self):
+		return None
+
+	def init_display(self):
+		self.pygame.display.update()
+
 	def update_display(self):
 		# TODO: pass rects to update()
 		self.pygame.display.update(rects)
 
 	def clear_updated(self):
 		self.updated = []
-
-	def set_running(self, running):
-		self.running = running
 
